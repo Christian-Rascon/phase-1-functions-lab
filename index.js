9126028111b7b4364bb8587bd7b54a37434c7ed5
Christian <christianrascon21@icloud.com>
2022-06-27 11:51:21 -0600

    commit tree 43faef6602b49f1300b45bddfd4e2aa85168fa42

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if (someValue<42) {
+        return (42-someValue);
+    }
+    else {
+        return (someValue-42);
+    }
+}
